38ab40cc65715ba776ad40c863702beec85fc8d0
Fikri Dzakir <fikridzakir179@gmail.com>
2021-02-17 14:20:09 +0700

    Arrow Function

---
+++ 3.1 Arrow Function/script.js
@@ -0,0 +1,11 @@
+const tampilNama = (nama) => {
+  return `halo ${nama}`;
+};
+
+// console.log(tampilNama("Erik"));
+
+let siswa = ["messi", "mbappe", "Ronaldo"];
+
+// let jumlahHuruf = siswa.map((nama) => nama.length);
+
+console.log(jumlahHuruf);
